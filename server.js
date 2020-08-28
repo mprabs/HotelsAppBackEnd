4860522fa7e4015e448d38696f1c7af15b7b0b19
mprabs <acharyaprabin03@gmail.com>
2020-08-28 14:59:19 +0545

    connection link

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 const mongoose = require('mongoose')
 var cors = require('cors');
 app.use(cors());
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const DATABASE_URL="mongodb+srv://dbUser:h8haBjtD56HpaNP@cluster0.vmnbl.mongodb.net/HotelsApp?retryWrites=true&w=majority"
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 db.on('error',(error) => console.error(error))
