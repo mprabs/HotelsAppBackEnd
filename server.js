b06bb1ca1ce99782534329c630d069faa3b8636a
mprabs <acharyaprabin03@gmail.com>
2020-09-19 12:20:20 +0545

    allow cors -- headers

--- server.js
+++ server.js
@@ -15,6 +15,25 @@ db.once('open',() => console.log('Connected to database !'))
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 const host = process.env.PORT ? '0.0.0.0' : 'localhost'
 const PORT = process.env.PORT || 8081
 
