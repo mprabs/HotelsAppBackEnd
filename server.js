9f479ff04dff7fc6de3265a8860daafe4c658743
mprabs <acharyaprabin03@gmail.com>
2021-02-22 10:47:37 +0545

    cors check

--- server.js
+++ server.js
@@ -1,53 +1,58 @@
-if (process.env.NODE_ENV !== 'production') { 
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const express = require('express');
-const app = express()
-const mongoose = require('mongoose')
-var cors = require('cors');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+var cors = require("cors");
 app.use(cors());
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error',(error) => console.error(error))
-db.once('open',() => console.log('Connected to database !'))
-
-app.use(express.json())
-
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-const host = process.env.PORT ? '0.0.0.0' : 'localhost'
-const PORT = process.env.PORT || 8081
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database !"));
 
-app.listen(PORT, host, () => console.log(process.env.PORT ? 'Server Port' : 'Local port'));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hotels API...');
-})
+// app.use(function (req, res, next) {
 
-const hotelsRouter = require('./routes/hotels')
-app.use('/hotels', hotelsRouter)
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-const loginRouter = require('./routes/users')
-app.use('/users', loginRouter)
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-const BookingRouter = require('./routes/bookings')
-app.use('/bookings', BookingRouter);
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     // res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
+
+// const host = process.env.PORT ? '0.0.0.0' : 'localhost'
+const PORT = process.env.PORT || 8081;
+
+app.listen(PORT, () =>
+  console.log(process.env.PORT ? "Server Port" : "Local port")
+);
+
+app.get("/", (req, res) => {
+  res.send("Hotels API. 2021...");
+});
+
+const hotelsRouter = require("./routes/hotels");
+app.use("/hotels", hotelsRouter);
+
+const loginRouter = require("./routes/users");
+app.use("/users", loginRouter);
+
+const BookingRouter = require("./routes/bookings");
+app.use("/bookings", BookingRouter);
